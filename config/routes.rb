96ba1c08c98f54ebf095608e2662e5b1191060c6
Lakysme Rocha <lakysme@gmail.com>
2023-04-03 18:22:57 +0100

    Authentication helper methods

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :users, except: [:new]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  get 'logout', to: 'sessions#destroy'
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 # Defines the root path route ("/")
