e5a9158895117195a0b0621fd8d7fce5e8339304
Lakysme Rocha <lakysme@gmail.com>
2023-04-13 18:03:14 +0100

    Integration test: Create category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
 
   def new
-
+    @category = Category.new
   end
 
   def index 
@@ -9,7 +9,23 @@ class CategoriesController < ApplicationController
   end
 
   def show 
+    @category = Category.find(params[:id])
+  end
 
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created"
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 
 end
