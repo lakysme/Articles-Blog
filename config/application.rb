f8f813b38e88513a9cfa96ae1dbd817fca336f27
Lakysme Rocha <lakysme@gmail.com>
2023-04-18 18:00:59 +0100

    Integration test and feature: listing categories

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module Blog
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    Rails.application.config.hosts << /.*\.ngrok-free\.app/
+    # Rails.application.config.hosts << /.*\.ngrok-free\.app/
   end
 end
